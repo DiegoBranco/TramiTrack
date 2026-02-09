f4e984a77d8471ea680877e491e0a431ed676e59
Diego Branco <diegoh89@gmail.com>
2026-02-09 07:34:50 -0400

    MongoDB Creado

--- TramiTrack/server/index.js
+++ TramiTrack/server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.SERVER_PORT || 3001;
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/tramitrack';
+
+mongoose.connect(mongoUri)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
 
 app.use(cors());
 app.use(express.json());
