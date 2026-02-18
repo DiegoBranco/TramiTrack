891c333e7e62fae00131269c87b1d2b0fe3be2a7
MaximilianoCar <maximilianoc2002@gmail.com>
2026-02-17 20:56:06 -0400

    error 2

--- tramitrack/Jenkinsfile.groovy
+++ tramitrack/Jenkinsfile.groovy
@@ -75,26 +75,23 @@ pipeline {
         stage('API Tests (Postman/Newman)') {
             steps {
                 script {
-                    // Entramos a la carpeta 'tramitrack' que es donde Jenkins clona el repo
                     dir('tramitrack') {
                         catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
-                        sh '''
-                        docker build -f tests/api/Dockerfile.test -t ${API_TEST_IMAGE} tests/api
-
-                        docker run --rm \
-                            --network tramitrack_default \
-                            ${API_TEST_IMAGE} \
-                            run coleccion.postman_collection.json \
-                            --env-var "url=http://express-server:3001" \
-                            --reporters cli
-                        '''
+                            sh '''
+                            # 1. Construir la imagen (necesario para tener la colección dentro)
+                            docker build -f tests/api/Dockerfile.test -t ${API_TEST_IMAGE} tests/api
+                            
+                            # 2. Ejecutar Newman forzando la URL manualmente
+                            # Esto ignora lo que diga la colección y usa esta URL directamente
+                            docker run --rm \
+                                --network tramitrack_default \
+                                ${API_TEST_IMAGE} \
+                                run coleccion.postman_collection.json \
+                                --url "http://express-server:3001" \
+                                --reporters cli
+                            '''
                         }
                     }
-                }
-            }
-            post {
-                unstable {
-                    echo 'AVISO: Las pruebas de API fallaron o los archivos no están presentes'
                 }
             }
         }
