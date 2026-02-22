74962eaca805d000e81b0e149a1d352a16f0f589
Diego Branco <diegoh89@gmail.com>
2026-02-22 08:59:14 -0400

    Pull

--- tramitrack/tests/unit/hello.test.js
+++
@@ -1,9 +0,0 @@
-describe('Hola mundo', () => {
-    test('suma básica', () => {
-        expect(1 + 1).toBe(2);
-    });
-
-    test('string contains', () => {
-        expect('hola mundo').toContain('hola');
-    });
-});
