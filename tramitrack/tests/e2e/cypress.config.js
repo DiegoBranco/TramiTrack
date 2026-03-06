8622ee82e228a11ca9709d9548c5d30ee6be67ce
MaximilianoCar <maximilianoc2002@gmail.com>
2026-03-05 21:44:33 -0400

    2e2 prueba

--- tramitrack/tests/e2e/cypress.config.js
+++ tramitrack/tests/e2e/cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_baseUrl || "http://localhost:3000",
     supportFile: false,
-    specPattern: "*.cy.js",
+    specPattern: "tests/e2e/*.cy.js",
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: true,
+    screenshotOnRunFailure: true,
   },
 });
